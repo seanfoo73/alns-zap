e6f60332b39d37b9fcb1dd29a021539770fe98c6
Sean Foo <seanfoo@cliff-ws-iphone.gateway.2wire.net>
2013-08-12 04:37:04 -0700

    Project ported to iphone ios5 retina

---
+++ ZapIOS/ZapIOS/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  ZapIOSAppController.h
+//  ZapIOS
+//
+//  Created by Sean Foo on 8/12/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ ZapIOS/ZapIOS/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  ZapIOSAppController.h
+//  ZapIOS
+//
+//  Created by Sean Foo on 8/12/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
